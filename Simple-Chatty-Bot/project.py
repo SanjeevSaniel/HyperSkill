72caeec8a5c90d72e73c61f3267d3ea714958258
Sanjeev Saniel Kujur <sanjeevsanielkujur7@outlook.com>
2020-08-06 13:04:34 +0530

    Add comments

--- Simple-Chatty-Bot/project.py
+++ Simple-Chatty-Bot/project.py
@@ -1,14 +1,17 @@
+# Greet message Function
 def greet(bot_name, birth_year):
     print('Hello! My name is ' + bot_name + '.')
     print('I was created in ' + birth_year + '.')
 
 
+# Function to remind for asking the name
 def remind_name():
     print('Please, remind me your name.')
     name = input()
     print('What a great name you have, ' + name + '!')
 
 
+# Function to guess the age using the input
 def guess_age():
     print('Let me guess your age.')
     print('Enter remainders of dividing your age by 3, 5 and 7.')
@@ -21,6 +24,7 @@ def guess_age():
     print("Your age is " + str(age) + "; that's a good time to start programming!")
 
 
+# Print all the counts until the user input range
 def count():
     print('Now I will prove to you that I can count to any number you want.')
     num = int(input())
